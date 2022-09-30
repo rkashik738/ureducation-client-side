c506e528cba347a6660b13c70bd72d6cdf6a781e
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-09-30 23:12:50 +0800

    organized project for development

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import SuccessHistory from './SuccessHistory';
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <SuccessHistory />
     </div>
   );
 };
