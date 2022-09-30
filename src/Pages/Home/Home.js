3a24b0fb404fce7f63b1de9ca2d10b3d7ac4f43d
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-09-30 23:21:50 +0800

    programs sections design added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Programs from './Programs';
 import SuccessHistory from './SuccessHistory';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Banner />
       <SuccessHistory />
+      <Programs />
     </div>
   );
 };
